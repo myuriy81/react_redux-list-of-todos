3ac028e4602837c13f2b60d097bab0552595e05a
Yurii Mosin <m.yuriy@icloud.com>
2025-04-27 21:16:54 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getTodos } from './api';
+import { todosSlice } from './features/todos';
+import { Todo } from './types/Todo';
+import { AppDispatch, RootState } from './app/store';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+export const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const dispatch = useDispatch<AppDispatch>();
+  const currTodos = useSelector(
+    (state: RootState) => state.currentTodo,
+  ) as Todo | null;
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const todoData = await getTodos();
 
-          <div className="block">
-            <Loader />
-            <TodoList />
+        dispatch(todosSlice.actions.todos(todoData as Todo[]));
+      } catch {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [dispatch]);
+
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
+
+            <div className="block">
+              <TodoFilter />
+            </div>
+            <div className="block">
+              {isLoading && !isError ? <Loader /> : <TodoList />}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-
-    <TodoModal />
-  </>
-);
+      {currTodos && <TodoModal />}
+    </>
+  );
+};
